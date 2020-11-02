5267612875de5b1ea97894a9982fca1f09fc78bd
SamGarner <samuel.w.garner@gmail.com>
2020-11-02 12:46:41 -0600

    move to recursion

--- knights_travails.rb
+++ knights_travails.rb
@@ -4,18 +4,36 @@ require 'pry'
 
 # knight class for movable chess piece
 class Knight
-  def initialize(current_location, parent = current_location)
+  attr_accessor :location, :parent_location
+
+  def initialize(current_location, parent = current_location, depth = 0)
     @location = current_location
     @parent_location = parent
+    # add a depth attribute/counter ?
+    @depth = depth 
   end
 end
 
+# input two coordinates
+# (Driver) - make sure coordinates are valid and within the Board grid (Board)
+# initialize Knight with current location as current location and parent
+# (loop/recurse)
+#   get list of allowable moves from Knight and init a new knight for each
+#   check if 'current location' of any of the new knights matches the end goal location
+#     if no, repeat this for all of the newly created knights
+#     if yes, count the number of returns up the return chain to get number of moves and 
+#         add parents to answer array of move/coordinates needed
+
 # class for 8x8 grid chess board
 class Board
-  attr_reader :next_move
+  attr_reader :next_move, :ending_coordinates
+  attr_accessor :knights
 
-  def initialize
+  def initialize(starting_coordinates, ending_coordinates)
     @board_array = Array.new(8, [0, 1, 2, 3, 4, 5, 6, 7])
+    @start = starting_coordinates
+    @end = ending_coordinates
+    @knights = []
   end
 
   def moves_from_current_point(current_point)
@@ -31,6 +49,24 @@ class Board
     next_move << [x - 2, y + 1]
     next_move << [x - 2, y - 1]
     next_move.select { |n| (0..7).include?(n[0]) && (0..7).include?(n[1]) }
+  end
+
+  def create_children(current_location, move_to_coordinates)
+    # move to coordinates = array of coordinate arrays
+    move_to_coordinates.each do |coordinates|
+      knights << Knight.new(coordinates, current_location)
+    end
+  end
+
+  def end_point_reached?(move_to_coordinates, ending_coordinates) #move to will be array of coords?
+    move_to_coordinates.any? { |coordinates| coordinates == ending_coordinates }
+  end
+
+  # def matches_endpoint(move_to_coordinates, ending_coordinates)
+  #   move_to_coordinates.select { |coordinates| coordinates == ending_coordinates }
+  # end
+  def end_point_knight(knights)
+    knights.select { |k| k.location = ending_coordinates }
   end
 
   def build_tree(next_move_array)
@@ -52,6 +88,48 @@ class Board
     end
   end
 end
+
+# Driver
+  # move from [0, 0] to [3, 3]
+p 'Input coordinates between 0, 0 and 7, 7 for the starting point of the knight'
+text_coordinates = gets.chomp.split(',')
+starting_point = text_coordinates.map(&:to_i)
+# input handling
+p 'Input coordinates between 0, 0 and 7, 7 for the end point of the knight'
+text_coordinates = gets.chomp.split(',')
+ending_point = text_coordinates.map(&:to_i)
+# input handling
+
+# starting_coordinates =  
+board = Board.new(starting_point, ending_point)
+root = Knight.new(starting_point, 'start')
+move_to_options = board.moves_from_current_point(root.location)
+board.create_children(root.location, move_to_options) #binding in here?
+binding.pry
+
+until move_to_options.includes?(ending_point) 
+  moves_from_current_point()
+end
+
+until board.end_point_reached?(move_to_options, ending_point)
+  move_to_options.each do |move|
+    create_children(move, board.moves_from_current_point(move))
+  end
+  move_to_options << board.moves_from_current_point
+
+  # next_moves = board.moves_from_current_point(move_to_options)
+  # next_moves.each do |move|
+  #   create_children(move)
+
+  board.end_point_knight(knights)
+
+binding.pry
+#BFS
+# queue = []
+# queue << root.location
+
+# if board.end_point_reached?
+
 
 # -should a 'node' have a 'left_value' and 'right_value' for the two coordinates or is that overcomplicating?
 
